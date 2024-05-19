55d6feadd38485f2c7f71f788d5220c78d44b336
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-19 21:58:41 +0300

    Swagger was integrated into the project.

--- src/main/java/dev/cey/VeterinaryProjectApplication.java
+++ src/main/java/dev/cey/VeterinaryProjectApplication.java
@@ -1,13 +1,16 @@
 package dev.cey;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Veterinary Management System", version = "1.0", description = "Veterinary Management System"))
 @SpringBootApplication
 public class VeterinaryProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VeterinaryProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinaryProjectApplication.class, args);
+    }
 
 }
