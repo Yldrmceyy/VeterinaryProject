42a602d5cbf1231e849d9333cc03ad3fd5922bae
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-23 14:55:15 +0300

    The Vaccine Controller and Vaccine Manager classes were updated.

--- src/main/java/dev/cey/api/VaccineController.java
+++ src/main/java/dev/cey/api/VaccineController.java
@@ -1,4 +1,60 @@
 package dev.cey.api;
 
+import dev.cey.business.abstracts.IVaccineService;
+import dev.cey.core.result.Result;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.VaccineSaveRequest;
+import dev.cey.dto.request.VaccineUpdateRequest;
+import dev.cey.dto.response.CursorResponse;
+import dev.cey.dto.response.VaccineResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/vaccines")
+@RequiredArgsConstructor
 public class VaccineController {
+    private final IVaccineService vaccineService;
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResultData<VaccineResponse> save(@Valid @RequestBody VaccineSaveRequest vaccineSaveRequest){
+        return this.vaccineService.save(vaccineSaveRequest);
+    }
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CursorResponse<VaccineResponse>> cursor(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize) {
+        return this.vaccineService.cursor(page, pageSize);
+    }
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<VaccineResponse> update(@Valid @RequestBody VaccineUpdateRequest vaccineUpdateRequest){
+        return this.vaccineService.update(vaccineUpdateRequest);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable Long id){
+        this.vaccineService.delete(id);
+        return ResultHelper.ok();
+    }
+    @GetMapping("/animal/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<List<VaccineResponse>> getVaccineByAnimalId(@PathVariable("id") Long animalId){
+        return this.vaccineService.findByAnimalId(animalId);
+    }
+    @GetMapping("/findByDate")
+    public ResultData<List<VaccineResponse>> getVaccinesByDate(
+            @RequestParam(name = "entryDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate entryDate,
+            @RequestParam(name = "exitDate") @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate exitDate
+    ){
+        return this.vaccineService.findByDate(entryDate,exitDate);
+    }
 }
--- src/main/java/dev/cey/business/impl/VaccineManager.java
+++ src/main/java/dev/cey/business/impl/VaccineManager.java
@@ -1,4 +1,104 @@
 package dev.cey.business.impl;
 
-public class VaccineManager {
+import dev.cey.business.abstracts.IAnimalService;
+import dev.cey.business.abstracts.IVaccineService;
+import dev.cey.core.config.ConvertEntityToResponse;
+import dev.cey.core.config.modelMapper.IModelMapperService;
+import dev.cey.core.exception.DataAlreadyExistException;
+import dev.cey.core.exception.NotFoundException;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.Msg;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.VaccineSaveRequest;
+import dev.cey.dto.request.VaccineUpdateRequest;
+import dev.cey.dto.response.CursorResponse;
+import dev.cey.dto.response.VaccineResponse;
+import dev.cey.entities.Animal;
+import dev.cey.entities.Vaccine;
+import dev.cey.repository.VaccineRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class VaccineManager implements IVaccineService {
+    private final VaccineRepo vaccineRepo;
+    private final IModelMapperService modelMapperService;
+    private final IAnimalService animalService;
+    private final ConvertEntityToResponse<Vaccine, VaccineResponse> convert;
+    @Override
+    public ResultData<VaccineResponse> save(VaccineSaveRequest vaccineSaveRequest) {
+
+        List<Vaccine> existVaccines = this.findByCodeAndName(vaccineSaveRequest.getCode(), vaccineSaveRequest.getName());
+        if (!existVaccines.isEmpty() && existVaccines.get(0).getProtectionFinishDate().isAfter(LocalDate.now())){
+            return ResultHelper.error("Aynı koda sahip aşının bitiş tarihi bitmemiş! ");
+        }
+        if (!existVaccines.isEmpty()){
+            throw new DataAlreadyExistException(Msg.getEntityForMsg(Vaccine.class));
+        }
+        Animal animal = this.animalService.get(vaccineSaveRequest.getAnimalId());
+        vaccineSaveRequest.setAnimalId(null);
+
+        Vaccine saveVaccine = this.modelMapperService.forRequest().map(vaccineSaveRequest, Vaccine.class);
+        saveVaccine.setAnimal(animal);
+
+        return ResultHelper.created(this.modelMapperService.forResponse().map(this.vaccineRepo.save(saveVaccine), VaccineResponse.class));
+    }
+
+    @Override
+    public Vaccine get(Long id) {
+        return this.vaccineRepo.findById((id)).orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND));
+    }
+
+    @Override
+    public ResultData<CursorResponse<VaccineResponse>> cursor(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Vaccine> vaccinePage = this.vaccineRepo.findAll(pageable);
+        Page<VaccineResponse> vaccineResponsePage = vaccinePage.map(vaccine -> this.modelMapperService.forResponse().map(vaccine, VaccineResponse.class));
+        return ResultHelper.cursor(vaccineResponsePage);
+    }
+
+    @Override
+    public ResultData<List<VaccineResponse>> findByAnimalId(Long id) {
+        List<Vaccine> vaccineList = this.vaccineRepo.findByAnimalId(id);
+        List<VaccineResponse> vaccineResponseList = this.convert.convertToResponseList(vaccineList, VaccineResponse.class);
+        return ResultHelper.success(vaccineResponseList);
+    }
+
+    @Override
+    public ResultData<List<VaccineResponse>> findByDate(LocalDate entryDate, LocalDate exitDate) {
+        List<Vaccine> vaccineList = this.vaccineRepo.findByprotectionFinishDateBetween(entryDate, exitDate);
+        List<VaccineResponse> vaccineResponseList = this.convert.convertToResponseList(vaccineList, VaccineResponse.class);
+        return ResultHelper.success(vaccineResponseList);
+    }
+
+    @Override
+    public List<Vaccine> findByCodeAndName(String code, String name) {
+        return this.vaccineRepo.findByCodeAndName(code,name);
+    }
+
+    @Override
+    public ResultData<VaccineResponse> update(VaccineUpdateRequest vaccineUpdateRequest) {
+        this.get(vaccineUpdateRequest.getId());
+        Vaccine updateVaccine = this.modelMapperService.forRequest().map(vaccineUpdateRequest, Vaccine.class);
+        return ResultHelper.success(this.modelMapperService.forResponse().map(updateVaccine, VaccineResponse.class));
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        Vaccine vaccine = this.get(id);
+        this.vaccineRepo.delete(vaccine);
+        return true;
+    }
+
+    public boolean isTrue(){
+        return true;
+    }
+
 }
