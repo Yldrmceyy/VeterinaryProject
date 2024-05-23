0fffd373f0e37ad3a402dd75d9b29954de4474fd
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-23 14:54:18 +0300

    The Customer Controller and Customer Manager classes were updated.

--- src/main/java/dev/cey/api/CustomerController.java
+++ src/main/java/dev/cey/api/CustomerController.java
@@ -1,4 +1,51 @@
 package dev.cey.api;
 
+import dev.cey.business.abstracts.ICustomerService;
+import dev.cey.core.result.Result;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.CustomerSaveRequest;
+import dev.cey.dto.request.CustomerUpdateRequest;
+import dev.cey.dto.response.CursorResponse;
+import dev.cey.dto.response.CustomerResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+    private final ICustomerService customerService;
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResultData<CustomerResponse> save(@Valid @RequestBody CustomerSaveRequest customerSaveRequest){
+        return this.customerService.save(customerSaveRequest);
+    }
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CustomerResponse> update(@Valid @RequestBody CustomerUpdateRequest customerUpdateRequest){
+        return this.customerService.update(customerUpdateRequest);
+    }
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CursorResponse<CustomerResponse>> cursor(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize) {
+        return this.customerService.cursor(page, pageSize);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable("id") Long id){
+        this.customerService.delete(id);
+        return ResultHelper.ok();
+    }
+    @GetMapping("/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<List<CustomerResponse>> get(@PathVariable("name") String name){
+        return this.customerService.findByName(name);
+    }
 }
--- src/main/java/dev/cey/business/impl/CustomerManager.java
+++ src/main/java/dev/cey/business/impl/CustomerManager.java
@@ -1,4 +1,84 @@
 package dev.cey.business.impl;
 
-public class CustomerManager {
+import dev.cey.business.abstracts.ICustomerService;
+import dev.cey.core.config.ConvertEntityToResponse;
+import dev.cey.core.config.modelMapper.IModelMapperService;
+import dev.cey.core.exception.DataAlreadyExistException;
+import dev.cey.core.exception.NotFoundException;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.Msg;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.CustomerSaveRequest;
+import dev.cey.dto.request.CustomerUpdateRequest;
+import dev.cey.dto.response.CursorResponse;
+import dev.cey.dto.response.CustomerResponse;
+import dev.cey.entities.Customer;
+import dev.cey.repository.CustomerRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerManager implements ICustomerService {
+    private final CustomerRepo customerRepo;
+    private final IModelMapperService modelMapperService;
+    private final ConvertEntityToResponse<Customer, CustomerResponse> convert;
+
+    @Override
+    public ResultData<CustomerResponse> save(CustomerSaveRequest customerSaveRequest) {
+        Customer saveCustomer = this.modelMapperService.forRequest().map(customerSaveRequest, Customer.class);
+        List<Customer> getByNamePhoneMail = this.findByNameAndMailAndPhone(
+                saveCustomer.getName(),
+                saveCustomer.getMail(),
+                saveCustomer.getPhone());
+        if (!getByNamePhoneMail.isEmpty()){
+            throw new DataAlreadyExistException(Msg.getEntityForMsg(Customer.class));
+        }
+        return ResultHelper.created(this.modelMapperService.forResponse().map(this.customerRepo.save(saveCustomer), CustomerResponse.class));
+    }
+
+    @Override
+    public Customer get(Long id) {
+        return this.customerRepo.findById((id)).orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND));
+    }
+
+    @Override
+    public ResultData<CursorResponse<CustomerResponse>> cursor(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Customer> customerPage = this.customerRepo.findAll(pageable);
+        Page<CustomerResponse> customerResponsePage = customerPage.map(customer -> this.modelMapperService.forResponse().map(customer, CustomerResponse.class));
+        return ResultHelper.cursor(customerResponsePage);
+    }
+
+    @Override
+    public ResultData<CustomerResponse> update(CustomerUpdateRequest customerUpdateRequest) {
+        this.get((customerUpdateRequest.getId()));
+        Customer updateCustomer = this.modelMapperService.forRequest().map(customerUpdateRequest, Customer.class);
+        return ResultHelper.success(this.modelMapperService.forResponse().map(this.customerRepo.save(updateCustomer), CustomerResponse.class));
+    }
+
+    @Override
+    public ResultData<List<CustomerResponse>> findByName(String name) {
+        List<Customer> customerList = this.customerRepo.findByName(name);
+        List<CustomerResponse> customerResponseList = this.convert.convertToResponseList(customerList, CustomerResponse.class);
+        return ResultHelper.success(customerResponseList);
+    }
+
+    @Override
+    public List<Customer> findByNameAndMailAndPhone(String name, String mail, String phone) {
+        return this.customerRepo.findByNameAndMailAndPhone(name, mail, phone);
+    }
+
+
+    @Override
+    public boolean delete(Long id) {
+        Customer customer = this.get(id);
+        this.customerRepo.delete(customer);
+        return true;
+    }
 }
