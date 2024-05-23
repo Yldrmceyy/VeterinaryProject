f35bbeb0a6f8af70940a9f39fee830aea2bb4329
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-23 14:54:55 +0300

    The Doctor Controller and Doctor Manager classes were updated.

--- src/main/java/dev/cey/api/DoctorController.java
+++ src/main/java/dev/cey/api/DoctorController.java
@@ -1,4 +1,44 @@
 package dev.cey.api;
 
+import dev.cey.business.abstracts.IDoctorService;
+import dev.cey.core.result.Result;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.DoctorSaveRequest;
+import dev.cey.dto.request.DoctorUpdateRequest;
+import dev.cey.dto.response.CursorResponse;
+import dev.cey.dto.response.DoctorResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/doctors")
+@RequiredArgsConstructor
 public class DoctorController {
+    private final IDoctorService doctorService;
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResultData<DoctorResponse> save(@Valid @RequestBody DoctorSaveRequest doctorSaveRequest){
+        return this.doctorService.save(doctorSaveRequest);
+    }
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<DoctorResponse> update(@Valid @RequestBody DoctorUpdateRequest doctorUpdateRequest){
+        return this.doctorService.update(doctorUpdateRequest);
+    }
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CursorResponse<DoctorResponse>> cursor(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize) {
+        return this.doctorService.cursor(page, pageSize);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable("id") Long id){
+        this.doctorService.delete(id);
+        return ResultHelper.ok();
+    }
 }
--- src/main/java/dev/cey/business/impl/DoctorManager.java
+++ src/main/java/dev/cey/business/impl/DoctorManager.java
@@ -1,4 +1,79 @@
 package dev.cey.business.impl;
 
-public class DoctorManager {
+import dev.cey.business.abstracts.IDoctorService;
+import dev.cey.core.config.modelMapper.IModelMapperService;
+import dev.cey.core.exception.DataAlreadyExistException;
+import dev.cey.core.exception.NotFoundException;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.Msg;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.DoctorSaveRequest;
+import dev.cey.dto.request.DoctorUpdateRequest;
+import dev.cey.dto.response.CursorResponse;
+import dev.cey.dto.response.DoctorResponse;
+import dev.cey.entities.Doctor;
+import dev.cey.repository.DoctorRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DoctorManager implements IDoctorService {
+    public final DoctorRepo doctorRepo;
+    public final IModelMapperService modelMapperService;
+    @Override
+    public ResultData<DoctorResponse> save(DoctorSaveRequest doctorSaveRequest) {
+        List<Doctor> doctorList = this.findByNameAndMailAndPhone(
+                doctorSaveRequest.getName(),
+                doctorSaveRequest.getMail(),
+                doctorSaveRequest.getPhone()
+        );
+        if (!doctorList.isEmpty()){
+            throw new DataAlreadyExistException(Msg.getEntityForMsg(Doctor.class));
+        }
+        Doctor saveDoctor = this.modelMapperService.forRequest().map(doctorSaveRequest, Doctor.class);
+        return ResultHelper.created(this.modelMapperService.forResponse().map(this.doctorRepo.save(saveDoctor), DoctorResponse.class));
+    }
+    @Override
+    public Doctor get(Long id) {
+        return doctorRepo.findById(id).orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND));
+    }
+    @Override
+    public ResultData<CursorResponse<DoctorResponse>> cursor(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Doctor> doctorPage =  this.doctorRepo.findAll(pageable);
+        Page<DoctorResponse> doctorResponsePage = doctorPage.map(doctor -> this.modelMapperService.forResponse().map(doctor, DoctorResponse.class));
+        return ResultHelper.cursor(doctorResponsePage);
+    }
+
+    @Override
+    public ResultData<DoctorResponse> update(DoctorUpdateRequest doctorUpdateRequest) {
+        this.get(doctorUpdateRequest.getId());
+        Doctor updateDoctor = this.modelMapperService.forRequest().map(doctorUpdateRequest, Doctor.class);
+        return ResultHelper.success(this.modelMapperService.forResponse().map(this.doctorRepo.save(updateDoctor), DoctorResponse.class));
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        Doctor doctor = this.get(id);
+        this.doctorRepo.delete(doctor);
+        return true;
+    }
+
+    @Override
+    public List<Doctor> findByIdAndAvailableDateDate(Long id, LocalDate localDate) {
+        return this.doctorRepo.findByIdAndAvailableDateDate(id, localDate);
+    }
+
+    @Override
+    public List<Doctor> findByNameAndMailAndPhone(String name, String mail, String phone) {
+        return this.doctorRepo.findByNameAndMailAndPhone(name, mail, phone);
+    }
 }
+
