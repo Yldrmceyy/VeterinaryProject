65677c747334a8988b97cbbc65e107371fde686e
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-23 14:54:38 +0300

    The AvailableDate Controller and AvailableDate Manager classes were updated.

--- src/main/java/dev/cey/api/AvailableDateController.java
+++ src/main/java/dev/cey/api/AvailableDateController.java
@@ -1,4 +1,45 @@
 package dev.cey.api;
 
+import dev.cey.business.abstracts.IAvailableDateService;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.result.Result;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.response.AvailableDateResponse;
+import dev.cey.dto.request.AvailableDateSaveRequest;
+import dev.cey.dto.request.AvailableDateUpdateRequest;
+import dev.cey.dto.response.CursorResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/v1/available-dates")
+@RequiredArgsConstructor
 public class AvailableDateController {
+    private final IAvailableDateService availableDateService;
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResultData<AvailableDateResponse> save(@Valid @RequestBody AvailableDateSaveRequest availableDateSaveRequest){
+        return this.availableDateService.save(availableDateSaveRequest);
+    }
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CursorResponse<AvailableDateResponse>> cursor(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize){
+        return this.availableDateService.cursor(page, pageSize);
+    }
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<AvailableDateResponse> update(@Valid @RequestBody AvailableDateUpdateRequest availableDateUpdateRequest){
+        return this.availableDateService.update(availableDateUpdateRequest);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable Long id){
+        this.availableDateService.delete(id);
+        return ResultHelper.ok();
+    }
 }
--- src/main/java/dev/cey/business/impl/AvailableDateManager.java
+++ src/main/java/dev/cey/business/impl/AvailableDateManager.java
@@ -1,4 +1,70 @@
 package dev.cey.business.impl;
 
-public class AvailableDateManager {
+import dev.cey.business.abstracts.IAvailableDateService;
+import dev.cey.core.config.modelMapper.IModelMapperService;
+import dev.cey.core.exception.DataAlreadyExistException;
+import dev.cey.core.exception.NotFoundException;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.Msg;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.AvailableDateSaveRequest;
+import dev.cey.dto.request.AvailableDateUpdateRequest;
+import dev.cey.dto.response.AvailableDateResponse;
+import dev.cey.dto.response.CursorResponse;
+import dev.cey.entities.AvailableDate;
+import dev.cey.repository.AvailableDateRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AvailableDateManager implements IAvailableDateService {
+    private final AvailableDateRepo availableDateRepo;
+    private final IModelMapperService modelMapperService;
+
+    @Override
+    public ResultData<AvailableDateResponse> save(AvailableDateSaveRequest availableDateSaveRequest) {
+        List<AvailableDate> availableDateList = this.availableDateRepo.findByDateAndDoctor(
+                availableDateSaveRequest.getDate(),
+                availableDateSaveRequest.getDoctor());
+        if (!availableDateList.isEmpty()) {
+            throw new DataAlreadyExistException(Msg.getEntityForMsg(AvailableDate.class));
+        }
+        AvailableDate saveAvailableDate = this.modelMapperService.forRequest().map(availableDateSaveRequest, AvailableDate.class);
+        return ResultHelper.created(this.modelMapperService.forResponse().map(this.availableDateRepo.save(saveAvailableDate), AvailableDateResponse.class));
+    }
+
+    @Override
+    public AvailableDate get(Long id) {
+        return this.availableDateRepo.findById(id).orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND));
+    }
+
+
+    @Override
+    public ResultData<CursorResponse<AvailableDateResponse>> cursor(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<AvailableDate> availableDatePage = this.availableDateRepo.findAll(pageable);
+        Page<AvailableDateResponse> availableDateResponsePage = availableDatePage.map(availableDate -> this.modelMapperService.forResponse().map(availableDate, AvailableDateResponse.class));
+        return ResultHelper.cursor(availableDateResponsePage);
+    }
+
+    @Override
+    public ResultData<AvailableDateResponse> update(AvailableDateUpdateRequest availableDateUpdateRequest) {
+        this.get(availableDateUpdateRequest.getId());
+        AvailableDate updateAvailableDate = this.modelMapperService.forResponse().map(availableDateUpdateRequest, AvailableDate.class);
+        return ResultHelper.success(this.modelMapperService.forResponse().map(this.availableDateRepo.save(updateAvailableDate), AvailableDateResponse.class));
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        AvailableDate availableDate = this.get(id);
+        this.availableDateRepo.delete(availableDate);
+        return true;
+    }
+
 }
