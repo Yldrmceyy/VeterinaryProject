ee63f4738a6b53991b8cdb3d51c78c9d772d76c8
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-23 14:49:47 +0300

    "Under the 'core' package, within the utilities package, msg and result helper classes were created.

--- src/main/java/dev/cey/core/utilies/Msg.java
+++ src/main/java/dev/cey/core/utilies/Msg.java
@@ -5,5 +5,8 @@ public class Msg {
     public static final String OK="İşlem Başarılı!";
     public static final String VALIDATE_ERROR="Veri Doğrulama Hatası!";
     public static final String NOT_FOUND="Veri Bulunamadı Hatası!";
+    public static String getEntityForMsg(Class<?> entity){
+        return entity.getSimpleName() + " Tablosunda Aynı Veri Bulunmaktadır.";
+    }
 }
 
