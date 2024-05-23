6fa189c3d7b83758f0812433a8b663a8858f6c66
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-23 14:51:24 +0300

    A method was added to the Vaccine Repo

--- src/main/java/dev/cey/repository/VaccineRepo.java
+++ src/main/java/dev/cey/repository/VaccineRepo.java
@@ -4,6 +4,12 @@ import dev.cey.entities.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface VaccineRepo extends JpaRepository<Vaccine, Long> {
+    List<Vaccine> findByAnimalId(Long id);
+    List<Vaccine> findByprotectionFinishDateBetween(LocalDate entryDate, LocalDate exitDate);
+    List<Vaccine> findByCodeAndName(String code, String name);
 }
