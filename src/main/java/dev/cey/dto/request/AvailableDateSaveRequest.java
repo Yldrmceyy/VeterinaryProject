467374b92ef62c9b6e764c5f8bda4fcbd03eca58
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-23 14:43:02 +0300

    Response and Request classes created for Animal, customer, doctor, vaccine, appointment, available Date

--- src/main/java/dev/cey/dto/request/AvailableDateSaveRequest.java
+++ src/main/java/dev/cey/dto/request/AvailableDateSaveRequest.java
@@ -1,7 +1,8 @@
 package dev.cey.dto.request;
 
+import dev.cey.entities.Doctor;
+
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -9,9 +10,9 @@ import java.time.LocalDate;
 @Data
 public class AvailableDateSaveRequest {
     @NotNull(message = "Available Date cannot be empty")
-    private LocalDate availableDate;
+    private LocalDate date;
     @NotNull(message = "Doctor id cannot be empty.")
-    @NotNull(message = "Doctor id cannot be null.")
-    @Positive(message = "Doctor id must be positive.")
-    private Long doctorId;
+
+   // private Long doctorId;
+    private Doctor doctor;
 }
--- src/main/java/dev/cey/dto/request/AvailableDateUpdateRequest.java
+++ src/main/java/dev/cey/dto/request/AvailableDateUpdateRequest.java
@@ -12,9 +12,6 @@ public class AvailableDateUpdateRequest {
     @Positive(message = "Available Date id must be positive.")
     private Long id;
     @NotNull(message = "Available Date Date cannot be empty.")
-    private LocalDate availableDate;
-    @NotNull(message = "Doctor id cannot be empty.")
-    @NotNull(message = "Doctor id cannot be null.")
-    @Positive(message = "Doctor id must be positive.")
-    private Long doctorId;
+    private LocalDate date;
+
 }
