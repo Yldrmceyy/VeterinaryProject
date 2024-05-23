fcd1b0f95822962a64df372f3d480072b3ab39f7
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-05-23 14:54:00 +0300

    The Animal Controller and Animal Manager classes were updated.

--- src/main/java/dev/cey/api/AnimalController.java
+++ src/main/java/dev/cey/api/AnimalController.java
@@ -1,4 +1,60 @@
 package dev.cey.api;
 
+import dev.cey.business.abstracts.IAnimalService;
+import dev.cey.core.result.Result;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.AnimalSaveRequest;
+import dev.cey.dto.request.AnimalUpdateRequest;
+import dev.cey.dto.response.AnimalResponse;
+import dev.cey.dto.response.CursorResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/animals")
+@RequiredArgsConstructor
 public class AnimalController {
+    private final IAnimalService animalService;
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResultData<AnimalResponse> save(@Valid @RequestBody AnimalSaveRequest animalSaveRequest){
+        return this.animalService.save(animalSaveRequest);
+    }
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CursorResponse<AnimalResponse>> cursor(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize){
+        return this.animalService.cursor(page, pageSize);
+    }
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<AnimalResponse> update(@Valid @RequestBody AnimalUpdateRequest animalUpdateRequest){
+        return this.animalService.update(animalUpdateRequest);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+        public Result delete(@PathVariable Long id){
+        this.animalService.delete(id);
+        return ResultHelper.ok();
+    }
+    @GetMapping("/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<List<AnimalResponse>> get(@PathVariable("name") String name){
+        return this.animalService.findByName(name);
+    }
+    @GetMapping("/customer/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<List<AnimalResponse>> getAnimalsByCustomerId(@PathVariable("id") Long customerId){
+        return this.animalService.findByCustomerId(customerId);
+    }
 }
+
+
+
--- src/main/java/dev/cey/business/impl/AnimalManager.java
+++ src/main/java/dev/cey/business/impl/AnimalManager.java
@@ -1,4 +1,98 @@
 package dev.cey.business.impl;
 
-public class AnimalManager {
+import dev.cey.business.abstracts.IAnimalService;
+import dev.cey.business.abstracts.ICustomerService;
+import dev.cey.core.config.ConvertEntityToResponse;
+import dev.cey.core.config.modelMapper.IModelMapperService;
+import dev.cey.core.exception.DataAlreadyExistException;
+import dev.cey.core.exception.NotFoundException;
+import dev.cey.core.result.ResultData;
+import dev.cey.core.utilies.Msg;
+import dev.cey.core.utilies.ResultHelper;
+import dev.cey.dto.request.AnimalSaveRequest;
+import dev.cey.dto.request.AnimalUpdateRequest;
+import dev.cey.dto.response.AnimalResponse;
+import dev.cey.dto.response.CursorResponse;
+import dev.cey.entities.Animal;
+import dev.cey.entities.Customer;
+import dev.cey.repository.AnimalRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class AnimalManager implements IAnimalService {
+    private final ConvertEntityToResponse<Animal, AnimalResponse> convert;
+    private final AnimalRepo animalRepo;
+    private final IModelMapperService modelMapperService;
+    private final ICustomerService customerService;
+
+    @Override
+    public ResultData<AnimalResponse> save(AnimalSaveRequest animalSaveRequest) {
+        Customer customer = this.customerService.get(animalSaveRequest.getCustomerId());
+        animalSaveRequest.setCustomerId(null);
+        Animal saveAnimal = this.modelMapperService.forRequest().map(animalSaveRequest, Animal.class);
+        saveAnimal.setCustomer(customer);
+        List<Animal> animalList = this.findByNameAndSpeciesAndBreedAndGender(
+                animalSaveRequest.getName(),
+                animalSaveRequest.getSpecies(),
+                animalSaveRequest.getBreed(),
+                animalSaveRequest.getGender()
+        );
+        if (!animalList.isEmpty()){
+            throw new DataAlreadyExistException(Msg.getEntityForMsg(Animal.class));
+        }
+        return ResultHelper.created(this.modelMapperService.forResponse().map(this.animalRepo.save(saveAnimal), AnimalResponse.class));
+    }
+    @Override
+    public Animal get(Long id) {
+        return this.animalRepo.findById((id)).orElseThrow(() -> new NotFoundException(Msg.NOT_FOUND));
+    }
+
+    @Override
+    public ResultData<CursorResponse<AnimalResponse>> cursor(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Animal> animalPage = this.animalRepo.findAll(pageable);
+        Page<AnimalResponse> animalResponsePage = animalPage.map(animal -> this.modelMapperService.forResponse().map(animal, AnimalResponse.class));
+        return ResultHelper.cursor(animalResponsePage);
+    }
+
+    @Override
+    public ResultData<List<AnimalResponse>> findByName(String name) {
+        List<Animal> animalList = this.animalRepo.findByName(name);
+        List<AnimalResponse> animalResponseList = this.convert.convertToResponseList(animalList, AnimalResponse.class);
+        return ResultHelper.success(animalResponseList);
+    }
+
+    @Override
+    public ResultData<List<AnimalResponse>> findByCustomerId(Long id) {
+        List<Animal> animalList = this.animalRepo.findByCustomerId(id);
+        List<AnimalResponse> animalResponseList = this.convert.convertToResponseList(animalList, AnimalResponse.class);
+        return ResultHelper.success(animalResponseList);
+    }
+
+    @Override
+    public List<Animal> findByNameAndSpeciesAndBreedAndGender(String name, String species, String breed, String gender) {
+        return this.animalRepo.findByNameAndSpeciesAndBreedAndGender(name, species, breed, gender);
+    }
+
+    @Override
+    public ResultData<AnimalResponse> update(AnimalUpdateRequest animalUpdateRequest) {
+        this.get(animalUpdateRequest.getId());
+        Animal updateAnimal = this.modelMapperService.forRequest().map(animalUpdateRequest, Animal.class);
+        this.animalRepo.save(updateAnimal);
+        return ResultHelper.success(this.modelMapperService.forResponse().map(updateAnimal, AnimalResponse.class));
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        Animal animal = this.get(id);
+        this.animalRepo.delete(animal);
+        return true;
+    }
 }
+
